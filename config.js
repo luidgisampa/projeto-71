656439ff2e7349c61f703f54dc80814e46ff21e8
Luidgi_Sampaio <65693951+luidgisampa@users.noreply.github.com>
2022-09-05 10:08:09 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaMKnezKF2zMx0LlNZkLz8EkbAJgfu-MI",
+    authDomain: "pro-71-a17ef.firebaseapp.com",
+    projectId: "pro-71-a17ef",
+    storageBucket: "pro-71-a17ef.appspot.com",
+    messagingSenderId: "817337212813",
+    appId: "1:817337212813:web:cfd19b6effaff89e6fa7c4"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
